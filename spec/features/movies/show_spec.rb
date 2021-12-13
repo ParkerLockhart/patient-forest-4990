4aba03020d58eaafff48906e1cb96276bd087138
Parker <88950699+ParkerLockhart@users.noreply.github.com>
2021-12-13 13:46:39 -0600

    Testing: US 2 feature tests

---
+++ spec/features/movies/show_spec.rb
@@ -0,0 +1,48 @@
+require 'rails_helper'
+
+RSpec.describe 'movie show page' do
+  before(:each) do
+    @universal = Studio.create!(name: 'Universal Studios', location: 'Hollywood')
+
+    @movie_1 = @universal.movies.create!(title: 'Raiders of the Lost Ark', creation_year: 1981, genre: 'Action/Adventure')
+    @movie_2 = @universal.movies.create!(title: 'Shrek', creation_year: 2001, genre: 'Comedy')
+  end
+
+  it 'shows movies titles' do
+    visit "/movies/#{@movie_1.id}"
+    expect(page).to have_content(@movie_1.title)
+    expect(page).to_not have_content(@movie_2.title)
+  end
+
+  it 'shows movies creation_year' do
+    visit "/movies/#{@movie_1.id}"
+
+    expect(page).to have_content(@movie_1.creation_year)
+  end
+
+  it 'shows movies genre' do
+    visit "/movies/#{@movie_1.id}"
+
+    expect(page).to have_content(@movie_1.genre)
+  end
+
+  xit 'shows movies actors' do 
+    visit "/movies/#{@movie_1.id}"
+
+    expect(page).to have_content(@actor_1)
+    expect(page).to have_content(@actor_2)
+  end
+
+  xit 'shows movies actors ordered by age asc' do
+    visit "/movies/#{@movie_1.id}"
+
+    expect(@actor_2).to appear_before(@actor_1)
+  end
+
+  xit 'shows average age of all actors' do
+    visit "/movies/#{@movie_1.id}"
+
+    expect(page).to have_content(@movie_1.actors.average_age)
+  end
+
+end
