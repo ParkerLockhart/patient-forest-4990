060825100cc3c135d8176cb805c77e4d532efe6f
Parker <88950699+ParkerLockhart@users.noreply.github.com>
2021-12-13 14:21:35 -0600

    Add seeds to check view on localhost

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,7 @@
 universal = Studio.create!(name: 'Universal Studios', location: 'Hollywood')
 
-universal.movies.create!(title: 'Raiders of the Lost Ark', creation_year: 1981, genre: 'Action/Adventure')
-universal.movies.create!(title: 'Shrek', creation_year: 2001, genre: 'Comedy')
+movie_1 = universal.movies.create!(title: 'Raiders of the Lost Ark', creation_year: 1981, genre: 'Action/Adventure')
+movie_2 = universal.movies.create!(title: 'Shrek', creation_year: 2001, genre: 'Comedy')
+
+actor_1 = movie_1.actors.create!(name: "Harrison Ford", age: 79)
+actor_2 = movie_1.actors.create!(name: "Cate Blanchett", age: 52)
