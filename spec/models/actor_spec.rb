e7f2706a978150ac7e137f7fd27c4bd76ed07a63
Parker <88950699+ParkerLockhart@users.noreply.github.com>
2021-12-13 13:47:32 -0600

    Add testing for actor model, movie model relation, and movie_actor model

--- spec/models/actor_spec.rb
+++ spec/models/actor_spec.rb
@@ -1,9 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe Movie, type: :model do
+RSpec.describe Actor, type: :model do
   describe 'relationships' do
-    it { should belong_to(:studio) }
     it { should have_many(:movie_actors) }
     it { should have_many(:actors).through(:movie_actors) }
   end
-end
+end 
